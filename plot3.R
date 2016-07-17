5ffb7db708e0eef1c708da1f1becd8d37bebb9a7
Navid Lambert-Shirzad <navidshirzd@gmail.com>
2016-07-17 13:33:56 -0700

    if the data does not exist, downloads the data and then creates the plots

--- plot3.R
+++ plot3.R
@@ -3,6 +3,16 @@
 ### By Navid Lambert-Shirzad
 
 setwd("C:/Users/NS/Desktop/Data_Science_Coursera/4. Exploratory Data Analysis/Week 1/Course Project 1")
+# Downloading and extracting files
+if (!file.exists("power.zip")){
+     url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+     download.file(url,"power.zip")
+} 
+
+if (!file.exists("household_power_consumption.txt")) { 
+     unzip("power.zip") 
+}
+
 # read data of 2007-02-01 and 2007-02-02
 data <- read.table("household_power_consumption.txt",skip=66637,nrows=2880,
                    col.names=c("Date","Time","Global_active_power","Global_reactive_power"
